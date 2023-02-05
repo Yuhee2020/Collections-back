e53d1ddaf7d5c01ffaa8cb73e9ecc1d7ebe5ce8c
Yukhimuk Dzmitry <yuhimuk.d@gmail.com>
2023-02-05 20:01:34 +0300

    client url refactoring

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app= express()
 app.use(express.json())
 app.use(cors({
     credentials:true,
-    origin: "https://course-project-front-rouge.vercel.app",
+    origin: "https://course-project-front-rouge.vercel.app" || "http://localhost:3000",
 }))
 app.use(cookieParser())
 app.use("/auth", authRouter)
