8201cd181e1d6985f4473cf4db0ed94d447cba31
Yukhimuk Dzmitry <yuhimuk.d@gmail.com>
2023-02-07 09:10:36 +0300

    cors refactoring

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app= express()
 app.use(express.json())
 app.use(cors({
     credentials:true,
-    origin: "http://localhost:3000",
+    origin: true,
 }))
 app.use(cookieParser())
 app.use("/auth", authRouter)
